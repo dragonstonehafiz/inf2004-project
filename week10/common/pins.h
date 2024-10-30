7ab52839354aada5c6e1c23f8338977ac69d5f07
dragonstonehafiz <dragonstonehafiz@gmail.com>
2024-10-30 19:31:31 +0800

    created seperate folders for week10 demo

---
+++ week10/common/pins.h
@@ -0,0 +1,12 @@
+#ifndef PINS_H_
+#define PINS_H_
+
+#define WHEEL_LEFT_PWN_PIN 10
+#define WHEEL_LEFT_OUT_PIN_1 8
+#define WHEEL_LEFT_OUT_PIN_2 9
+
+#define WHEEL_RIGHT_PWN_PIN 11
+#define WHEEL_RIGHT_OUT_PIN_1 2
+#define WHEEL_RIGHT_OUT_PIN_2 3
+
+#endif
