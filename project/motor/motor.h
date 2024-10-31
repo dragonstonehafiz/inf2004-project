2d9d758896cc55a71ccec451157ba3b56f514f65
dragonstonehafiz <dragonstonehafiz@gmail.com>
2024-10-31 11:54:02 +0800

    updated motor code for example

--- project/motor/motor.h
+++ project/motor/motor.h
@@ -4,50 +4,53 @@
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
 #include <stdio.h>
-
-#define WHEEL_LEFT_PWN_PIN 10
-#define WHEEL_LEFT_OUT_PIN_1 8
-#define WHEEL_LEFT_OUT_PIN_2 9
-
-#define WHEEL_RIGHT_PWN_PIN 11
-#define WHEEL_RIGHT_OUT_PIN_1 2
-#define WHEEL_RIGHT_OUT_PIN_2 3
+#include "pins.h"
 
 #define CLOCK_FREQ 125000000
 #define PWM_WRAP 65535
 #define PWM_FREQ 25
 #define PWM_DIVIDER ((float)CLOCK_FREQ / (PWM_WRAP * PWM_FREQ))
 
-// true = clockwise
-bool left_wheel_dir = true;
-bool right_wheel_dir = true;
-
 // PID Variables
 float Kp = 0.1, Ki = 0.01, Kd = 0.005;
 
 void init_motor();
+void init_left_motor();
+void init_right_motor();
+
 void setup_pwm(uint pwm_pin, float duty_cycle);
 void set_pwm_duty_cycle(uint pwm_pin, float duty_cycle);
+
 void compute_pid(float target_speed, float current_speed, float *duty_cycle, float *integral, float *prev_error);
 
+void turn_wheel_clockwise(uint pwm_pin);
+void turn_wheel_anticlockwise(uint pwm_pin);
+void set_wheel_duty_cycle(uint pwm_pin, float *dutyCycle);
+
 void init_motor()
+{    
+    init_left_motor();
+    init_right_motor();
+}
+void init_left_motor()
+{
+    setup_pwm(WHEEL_LEFT_PWN_PIN, 0.f);
+    gpio_init(WHEEL_LEFT_OUT_PIN_1);
+    gpio_init(WHEEL_LEFT_OUT_PIN_2);
+    gpio_set_dir(WHEEL_LEFT_OUT_PIN_1, GPIO_OUT);
+    gpio_set_dir(WHEEL_LEFT_OUT_PIN_2, GPIO_OUT);
+    turn_wheel_clockwise(WHEEL_LEFT_PWN_PIN);
+}
+void init_right_motor()
 {
     setup_pwm(WHEEL_RIGHT_PWN_PIN, 0.f);
     gpio_init(WHEEL_RIGHT_OUT_PIN_1);
     gpio_init(WHEEL_RIGHT_OUT_PIN_2);
     gpio_set_dir(WHEEL_RIGHT_OUT_PIN_1, GPIO_OUT);
     gpio_set_dir(WHEEL_RIGHT_OUT_PIN_2, GPIO_OUT);
-    gpio_put(WHEEL_RIGHT_OUT_PIN_1, right_wheel_dir);
-    gpio_put(WHEEL_RIGHT_OUT_PIN_2, !right_wheel_dir);
-
-    setup_pwm(WHEEL_LEFT_PWN_PIN, 0.f);
-    gpio_init(WHEEL_LEFT_OUT_PIN_1);
-    gpio_init(WHEEL_LEFT_OUT_PIN_2);
-    gpio_set_dir(WHEEL_LEFT_OUT_PIN_1, GPIO_OUT);
-    gpio_set_dir(WHEEL_LEFT_OUT_PIN_2, GPIO_OUT);
-    gpio_put(WHEEL_LEFT_OUT_PIN_1, left_wheel_dir);
-    gpio_put(WHEEL_LEFT_OUT_PIN_2, !left_wheel_dir);
+    turn_wheel_clockwise(WHEEL_RIGHT_PWN_PIN);
 }
+
 void setup_pwm(uint pwm_pin, float duty_cycle) 
 {
     // Set the GPIO function to PWM
@@ -81,5 +84,38 @@ void compute_pid(float target_speed, float current_speed, float *duty_cycle, flo
     *prev_error = error;
 }
 
+void turn_wheel_clockwise(uint pwm_pin)
+{
+    if (pwm_pin == WHEEL_LEFT_PWN_PIN)
+    {
+        printf("left forward\n");
+        gpio_put(WHEEL_LEFT_OUT_PIN_1, true);
+        gpio_put(WHEEL_LEFT_OUT_PIN_2, false);
+    } 
+    else if (pwm_pin == WHEEL_RIGHT_PWN_PIN)
+    {
+        printf("right forward\n");
+        gpio_put(WHEEL_RIGHT_OUT_PIN_1, true);
+        gpio_put(WHEEL_RIGHT_OUT_PIN_2, false);
+    }
+}
+void turn_wheel_anticlockwise(uint pwm_pin)
+{
+    if (pwm_pin == WHEEL_LEFT_PWN_PIN)
+    {
+        gpio_put(WHEEL_LEFT_OUT_PIN_1, false);
+        gpio_put(WHEEL_LEFT_OUT_PIN_2, true);
+    } 
+    else if (pwm_pin == WHEEL_RIGHT_PWN_PIN)
+    {
+        gpio_put(WHEEL_RIGHT_OUT_PIN_1, false);
+        gpio_put(WHEEL_RIGHT_OUT_PIN_2, true);
+    }
+}
+void set_wheel_duty_cycle(uint pwm_pin, float* dutyCycle)
+{
+    if (pwm_pin == WHEEL_LEFT_PWN_PIN || pwm_pin == WHEEL_RIGHT_PWN_PIN)
+        set_pwm_duty_cycle(pwm_pin, *dutyCycle);
+}
 
 #endif
--- project/motor/project-motor-example.c
+++ project/motor/project-motor-example.c
@@ -2,76 +2,61 @@
 #include "hardware/pwm.h"
 #include <stdio.h>
 #include "motor.h"
-#include "wheel_encoder.h"
 
-#define BTN_1_PIN 20
-#define BTN_2_PIN 21
-#define BTN_3_PIN 22
+#define BTN_20_PIN 20
+#define BTN_21_PIN 21
+#define BTN_23_PIN 22
 
-uint8_t duty_cycle = 0;
-uint64_t last_time_r;
+float duty_cycle = 0.0f;
 
 void init_gpio();
-
 void irq_handler(uint gpio, uint32_t events);
-
 void irq_btn(uint gpio);
 
 int main() 
 {
     init_gpio();
 
-    float target_speed = 25.f;
-    float integral = 0;
-    float prev_error = 0;
-
     while (true) 
     {
         tight_loop_contents();
-        // compute_pid(&target_speed, &current_speed, &integral, &prev_error);
     }
 }
 
 void init_gpio() 
 {
     stdio_init_all();
-
     init_motor();
-    init_wheel_encoder();
 
-    gpio_set_irq_enabled_with_callback(WHEEL_ENCODER_RIGHT_PIN, GPIO_IRQ_EDGE_FALL, true, &irq_handler);
-    gpio_set_irq_enabled_with_callback(BTN_1_PIN, GPIO_IRQ_EDGE_FALL, true, &irq_handler);
-    gpio_set_irq_enabled_with_callback(BTN_2_PIN, GPIO_IRQ_EDGE_FALL, true, &irq_handler);
-    gpio_set_irq_enabled_with_callback(BTN_3_PIN, GPIO_IRQ_EDGE_FALL, true, &irq_handler);
+    gpio_set_irq_enabled_with_callback(BTN_20_PIN, GPIO_IRQ_EDGE_FALL, true, &irq_handler);
+    gpio_set_irq_enabled_with_callback(BTN_21_PIN, GPIO_IRQ_EDGE_FALL, true, &irq_handler);
+    gpio_set_irq_enabled_with_callback(BTN_23_PIN, GPIO_IRQ_EDGE_FALL, true, &irq_handler);
 }
-
 void irq_handler(uint gpio, uint32_t events)
 {
-    if (gpio == BTN_1_PIN || gpio == BTN_2_PIN || gpio == BTN_3_PIN)
+    if (gpio == BTN_20_PIN || gpio == BTN_21_PIN || gpio == BTN_23_PIN)
         irq_btn(gpio);
-    else if (gpio == WHEEL_ENCODER_RIGHT_PIN)
-        irq_ir_sensor(WHEEL_ENCODER_RIGHT_PIN);
 }
-
 void irq_btn(uint gpio)
 {
-    if (gpio == BTN_1_PIN) {
-        left_wheel_dir = !left_wheel_dir;
-        gpio_put(WHEEL_LEFT_OUT_PIN_1, left_wheel_dir);
-        gpio_put(WHEEL_LEFT_OUT_PIN_2, !left_wheel_dir);
+    if (gpio == BTN_20_PIN) 
+    {
+        turn_wheel_anticlockwise(WHEEL_LEFT_PWN_PIN);
+        turn_wheel_clockwise(WHEEL_RIGHT_PWN_PIN);
     }
-    else if (gpio == BTN_2_PIN) {
-        duty_cycle += 25;
-        if (duty_cycle > 100)
-            duty_cycle = 0;
-        // Divide by 101 because when duty cycle is 100, motor won't turn
-        float duty_cycle_percent = duty_cycle / 101.f;
-        set_pwm_duty_cycle(WHEEL_LEFT_PWN_PIN, duty_cycle_percent);
-        set_pwm_duty_cycle(WHEEL_RIGHT_PWN_PIN, duty_cycle_percent);
+    else if (gpio == BTN_21_PIN) 
+    {
+        if (duty_cycle >= 0.75f)
+            duty_cycle = 0.f;
+        else
+            duty_cycle = 0.75f;
+
+        set_wheel_duty_cycle(WHEEL_RIGHT_PWN_PIN, &duty_cycle);
+        set_wheel_duty_cycle(WHEEL_LEFT_PWN_PIN, &duty_cycle);
     }
-    else if (gpio == BTN_3_PIN) {
-        right_wheel_dir = !right_wheel_dir;
-        gpio_put(WHEEL_RIGHT_OUT_PIN_1, right_wheel_dir);
-        gpio_put(WHEEL_RIGHT_OUT_PIN_2, !right_wheel_dir);
+    else if (gpio == BTN_23_PIN) 
+    {
+        turn_wheel_clockwise(WHEEL_LEFT_PWN_PIN);
+        turn_wheel_anticlockwise(WHEEL_RIGHT_PWN_PIN);
     }
 }
