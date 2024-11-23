05fa6f18f3a97a4e740d7f1c36a834151b1f7540
dragonstonehafiz <dragonstonehafiz@gmail.com>
2024-11-23 15:33:29 +0800

    bugfix for control mode

--- project/main/states.c
+++ project/main/states.c
@@ -9,28 +9,7 @@ volatile bool tooClose = false;
 volatile uint64_t ultrasonicLastCheckTime;
 
 void handleControls(movement_data_t *movementData);
-void handleUltrasonic()
-{
-    uint64_t currentTime = time_us_64();
-    uint64_t timeDiff = currentTime - ultrasonicLastCheckTime;
-
-    // timediff larger than 100ms
-    if (timeDiff > 100000)
-    {
-        triggerPulse();
-
-        sleep_ms(30);
-
-        float calculatedDist = getCm();
-        // printf("calculatedDist: %.2f\n", calculatedDist);
-        if (calculatedDist <= 25.f && calculatedDist >= 0.f)
-            tooClose = true;
-        else
-            tooClose = false;
-        
-        ultrasonicLastCheckTime = currentTime;
-    }
-}
+void handleUltrasonic();
 
 void changeState(uint8_t nextState)
 {
@@ -117,12 +96,20 @@ void handleControls(movement_data_t *movementData)
         // 皮肉じゃないよ
         // You can only move forward/backward or turn.
         // You cannot turn while going forward/backward. Why? Because I give up, that's why.
-        if (movementData->forward_direction == 'F' && !tooClose)
-        {
-            set_car_state(CAR_FORWARD);
-            float dutyCycle = minimumDutyCycle + remainder * movementData->forward_percentage;
-            set_wheels_duty_cycle(dutyCycle);
-            pid_right.enabled = true;
+        if (movementData->forward_direction == 'F')
+        {  
+            if (!tooClose)
+            {
+                set_car_state(CAR_FORWARD);
+                float dutyCycle = minimumDutyCycle + remainder * movementData->forward_percentage;
+                set_wheels_duty_cycle(dutyCycle);
+                pid_right.enabled = true;
+            }
+            else
+            {   
+                set_car_state(CAR_STATIONARY);
+                pid_right.enabled = false;
+            }
         }
         else if (movementData->forward_direction == 'B')
         {
@@ -144,5 +131,27 @@ void handleControls(movement_data_t *movementData)
                 set_car_state(CAR_STATIONARY);
             set_wheels_duty_cycle(movementData->turn_percentage);
         }
+    }
+}
+void handleUltrasonic()
+{
+    uint64_t currentTime = time_us_64();
+    uint64_t timeDiff = currentTime - ultrasonicLastCheckTime;
+
+    // timediff larger than 100ms
+    if (timeDiff > 100000)
+    {
+        triggerPulse();
+
+        sleep_ms(30);
+
+        float calculatedDist = getCm();
+        // printf("calculatedDist: %.2f\n", calculatedDist);
+        if (calculatedDist <= 25.f && calculatedDist >= 0.f)
+            tooClose = true;
+        else
+            tooClose = false;
+        
+        ultrasonicLastCheckTime = currentTime;
     }
 }
